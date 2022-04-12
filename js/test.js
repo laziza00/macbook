3b088c4341da44d27512282fb498f6ba0cb19adc
laziza@mail.ru <laziza00>
2022-04-12 08:37:00 +0500

    initial commit

---
+++ js/test.js
@@ -0,0 +1,139 @@
+const macBook = [
+    {
+        id: 1,
+        nomi: `Gold`,
+        imgUrl: "../images/macbookAir.webp",
+        imgSlider: ["../images/macbookAir.webp", "../images/macPink2.jpg", "../images/macPink3.jpg", "../images/macPink4.jpg", "../images/macPink5.jpg"],
+        ram: [
+            {
+                GB: 8,
+                xotira: [
+                    {
+                        name: `M1/8/256 Gold`,
+                        gbXotira: 256,
+                        price: 12497000
+                    },
+                    {
+                        name: `M1/8/512 Gold`,
+                        gbXotira: 512,
+                        price: 15066000
+                    }
+                ]
+            }, 
+            {
+                GB: 16,
+                xotira: [
+                    {
+                        name: `M1/16/256 Gold`,
+                        gbXotira: 256,
+                        price: `16 935 000`
+                    },
+                    {
+                        name: `M1/16/512 Gold`,
+                        gbXotira: 512,
+                        price: `19 270 500`
+                    },
+                    {
+                        name: `M1/16/1tb Gold`,
+                        gbXotira: 1,
+                        price: `22 497 000`
+                    }
+                ]
+            }
+        ],
+
+    },
+    {
+        id: 2,
+        nomi: `Silver`,
+        imgUrl: "../images/macSilver1.jpg",
+        imgSlider: ["../images/macSilver1.jpg", "../images/macSilver2.jpg", "../images/macSilver3.jpg", "../images/macSilver4.jpg", "../images/macSilver5.jpg"],
+        ram: [
+            {
+                GB: 8,
+                xotira: [
+                    {
+                        name: `M1/8/256 Silver`,
+                        gbXotira: 256,
+                        price: 12497000
+                    },
+                    {
+                        name: `M1/8/512 Silver`,
+                        gbXotira: 512,
+                        price: 15066000
+                    },
+                    {
+                        name: `M1/16/1tb Silver`,
+                        gbXotira: 1,
+                        price: `22 497 000`
+                    }
+                ]
+            }, 
+            {
+                GB: 16,
+                xotira: [
+                    {
+                        name: `M1/16/256 Silver`,
+                        gbXotira: 256,
+                        price: `16 935 000`
+                    },
+                    {
+                        name: `M1/16/512 Silver`,
+                        gbXotira: 512,
+                        price: `19 270 500`
+                    },
+                    {
+                        name: `M1/16/1tb Silver`,
+                        gbXotira: 1,
+                        price: `22 497 000`
+                    }
+                ]
+            }
+        ],
+
+    },
+    {
+        id: 3,
+        nomi: `Grey`,
+        imgUrl: "../images/macGrey1.jpg",
+        imgSlider: ["../images/macGrey1.jpg", "../images/macGrey2.jpg", "../images/macGrey3.jpg", "../images/macGrey4.jpg", "../images/macGrey5.jpg"],
+        ram: [
+            {
+                GB: 8,
+                xotira: [
+                    {
+                        name: `M1/8/256 Grey`,
+                        gbXotira: 256,
+                        price: 12497000
+                    },
+                    {
+                        name: `M1/8/512 Grey`,
+                        gbXotira: 512,
+                        price: 15066000
+                    }
+                ]
+            }, 
+            {
+                GB: 16,
+                xotira: [
+                    {
+                        name: `M1/16/256 Grey`,
+                        gbXotira: 256,
+                        price: `16 935 000`
+                    },
+                    {
+                        name: `M1/16/512 Grey`,
+                        gbXotira: 512,
+                        price: `19 270 500`
+                    },
+                    {
+                        name: `M1/16/1tb Grey`,
+                        gbXotira: 1,
+                        price: `22 497 000`
+                    }
+                ]
+            }
+        ],
+
+    }
+];
