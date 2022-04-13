a95cea2a9ec9d75c541a88328849716733b22531
laziza@mail.ru <laziza00>
2022-04-13 08:04:54 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,140 +1,140 @@
-let imgBtn1 = document.querySelector('.img-btn1');
-let imgBtn2 = document.querySelector('.img-btn2');
-let imgBtn3 = document.querySelector('.img-btn3');
-let imgBtn4 = document.querySelector('.img-btn4');
-let imgBtn5 = document.querySelector('.img-btn5');
+// let imgBtn1 = document.querySelector('.img-btn1');
+// let imgBtn2 = document.querySelector('.img-btn2');
+// let imgBtn3 = document.querySelector('.img-btn3');
+// let imgBtn4 = document.querySelector('.img-btn4');
+// let imgBtn5 = document.querySelector('.img-btn5');
 
-let macSliderBox = document.querySelectorAll('.mac__slider-box img')
-let macSlider = document.querySelector('.mac__slider-box')
+// let macSliderBox = document.querySelectorAll('.mac__slider-box img')
+// let macSlider = document.querySelector('.mac__slider-box')
 
-count =0
-px = 490
+// count =0
+// px = 490
 
-imgBtn2.addEventListener('click', ()=> {
-    count =1;
-    macSlider.style.transform = `translateX(-${px}px)`
-    imgBtn2.classList.add('act-border')
-    imgBtn1.classList.remove('act-border')
-    imgBtn3.classList.remove('act-border')
-    imgBtn4.classList.remove('act-border')
-    imgBtn5.classList.remove('act-border')
-})
-imgBtn3.addEventListener('click', ()=> {
-    count =2;
-    macSlider.style.transform = `translateX(-${px*count}px)`
-    imgBtn3.classList.add('act-border')
-    imgBtn2.classList.remove('act-border')
-    imgBtn1.classList.remove('act-border')
-    imgBtn4.classList.remove('act-border')
-    imgBtn5.classList.remove('act-border')
-})
-imgBtn4.addEventListener('click', ()=> {
-    count =3;
-    macSlider.style.transform = `translateX(-${px*count}px)`
-    imgBtn4.classList.add('act-border')
-    imgBtn3.classList.remove('act-border')
-    imgBtn2.classList.remove('act-border')
-    imgBtn1.classList.remove('act-border')
-    imgBtn5.classList.remove('act-border')
-})
-imgBtn5.addEventListener('click', ()=> {
-    count =4;
-    macSlider.style.transform = `translateX(-${px*count}px)`
-    imgBtn5.classList.add('act-border')
-    imgBtn4.classList.remove('act-border')
-    imgBtn3.classList.remove('act-border')
-    imgBtn2.classList.remove('act-border')
-    imgBtn1.classList.remove('act-border')
-})
-imgBtn1.addEventListener('click', ()=> {
-    count =0;
-    macSlider.style.transform = `translateX(${px*count}px)`
-    imgBtn1.classList.add('act-border')
-    imgBtn5.classList.remove('act-border')
-    imgBtn3.classList.remove('act-border')
-    imgBtn2.classList.remove('act-border')
-    imgBtn1.classList.remove('act-border')
-    imgBtn4.classList.remove('act-border')
-})
-
-
+// imgBtn2.addEventListener('click', ()=> {
+//     count =1;
+//     macSlider.style.transform = `translateX(-${px}px)`
+//     imgBtn2.classList.add('act-border')
+//     imgBtn1.classList.remove('act-border')
+//     imgBtn3.classList.remove('act-border')
+//     imgBtn4.classList.remove('act-border')
+//     imgBtn5.classList.remove('act-border')
+// })
+// imgBtn3.addEventListener('click', ()=> {
+//     count =2;
+//     macSlider.style.transform = `translateX(-${px*count}px)`
+//     imgBtn3.classList.add('act-border')
+//     imgBtn2.classList.remove('act-border')
+//     imgBtn1.classList.remove('act-border')
+//     imgBtn4.classList.remove('act-border')
+//     imgBtn5.classList.remove('act-border')
+// })
+// imgBtn4.addEventListener('click', ()=> {
+//     count =3;
+//     macSlider.style.transform = `translateX(-${px*count}px)`
+//     imgBtn4.classList.add('act-border')
+//     imgBtn3.classList.remove('act-border')
+//     imgBtn2.classList.remove('act-border')
+//     imgBtn1.classList.remove('act-border')
+//     imgBtn5.classList.remove('act-border')
+// })
+// imgBtn5.addEventListener('click', ()=> {
+//     count =4;
+//     macSlider.style.transform = `translateX(-${px*count}px)`
+//     imgBtn5.classList.add('act-border')
+//     imgBtn4.classList.remove('act-border')
+//     imgBtn3.classList.remove('act-border')
+//     imgBtn2.classList.remove('act-border')
+//     imgBtn1.classList.remove('act-border')
+// })
+// imgBtn1.addEventListener('click', ()=> {
+//     count =0;
+//     macSlider.style.transform = `translateX(${px*count}px)`
+//     imgBtn1.classList.add('act-border')
+//     imgBtn5.classList.remove('act-border')
+//     imgBtn3.classList.remove('act-border')
+//     imgBtn2.classList.remove('act-border')
+//     imgBtn1.classList.remove('act-border')
+//     imgBtn4.classList.remove('act-border')
+// })
 
 
-let btn16gb = document.querySelector('.btn16gb')
-let btn8gb = document.querySelector('.btn8gb')
-let btn256gb = document.querySelector('.btn256gb')
-let btn512gb = document.querySelector('.btn512gb')
-let btn1tb = document.querySelector('.btn1tb')
-let btngold = document.querySelector('.btngold')
-let btngray = document.querySelector('.btngray')
-let btngraycosmos = document.querySelector('.btngraycosmos')
-let macPrice = document.querySelector('.mac__price')
-let macPrice2 = document.querySelector('.mac__price2')
 
-count2 =0
 
-btn16gb.addEventListener('click', ()=> {
-    btn1tb.classList.remove('d-none')
-    btn16gb.classList.add('active')
-    btn8gb.classList.remove('active')
-    macPrice.textContent ="16 935 000"
-    macPrice2.textContent= "19 813 500"
-})
-btn8gb.addEventListener('click', ()=> {
-    btn16gb.classList.remove('active')
-    btn8gb.classList.add('active')
-    btn1tb.classList.add('d-none')
-    macPrice.textContent ="12 497 000"
-    macPrice2.textContent= "14 621 000"
-})
-btn512gb.addEventListener('click', ()=> {
-    btn256gb.classList.remove('active')
-    btn1tb.classList.remove('active')
-    btn512gb.classList.add('active')
-    btn1tb.classList.remove('d-none')
-    macPrice.textContent ="19 270 500"
-    macPrice2.textContent= "22 546 500"
-    count2 =1
-})
-btn1tb.addEventListener('click', ()=> {
-    count2 =0
-    btn512gb.classList.remove('active')
-    btn1tb.classList.add('active')
-    btn8gb.classList.add('d-none')
-    btn16gb.classList.add('active')
-    macPrice.textContent ="20 438 500"
-    macPrice2.textContent= "23 913 000"
-    // count2 =1
-    if(count2==1) {
-        btn512gb.classList.add('active')
-        btn1tb.classList.remove('active')
-        btn8gb.classList.remove('d-none')
-        btn16gb.classList.remove('active')
-    }
-})
-btn256gb.addEventListener('click', ()=> {
-    btn512gb.classList.remove('active')
-    btn256gb.classList.add('active')
-    btn1tb.classList.remove('active')
-    btn8gb.classList.remove('d-none')
-    btn16gb.classList.remove('active')
-    macPrice.textContent ="16 935 000"
-    macPrice2.textContent= "19 813 500"
-})
+// let btn16gb = document.querySelector('.btn16gb')
+// let btn8gb = document.querySelector('.btn8gb')
+// let btn256gb = document.querySelector('.btn256gb')
+// let btn512gb = document.querySelector('.btn512gb')
+// let btn1tb = document.querySelector('.btn1tb')
+// let btngold = document.querySelector('.btngold')
+// let btngray = document.querySelector('.btngray')
+// let btngraycosmos = document.querySelector('.btngraycosmos')
+// let macPrice = document.querySelector('.mac__price')
+// let macPrice2 = document.querySelector('.mac__price2')
 
-let plusBtn = document.querySelector('.plusBtn')
-let minusBtn = document.querySelector('.minusBtn')
-let macFullBtn =document.querySelector('.mac__full-btn')
+// count2 =0
 
-plusBtn.addEventListener('click', ()=> {
-    let a = macPrice.textContent*2
-    console.log(a);
-    macPrice.textContent = a
-    macPrice2.textContent= `${macPrice/2}`
-})
-minusBtn.addEventListener('click', ()=> {
-    let a = macPrice.textContent*2
-    console.log(a);
-    macPrice.textContent = a
-    macPrice2.textContent= `${macPrice/2}`
-})
+// btn16gb.addEventListener('click', ()=> {
+//     btn1tb.classList.remove('d-none')
+//     btn16gb.classList.add('active')
+//     btn8gb.classList.remove('active')
+//     macPrice.textContent ="16 935 000"
+//     macPrice2.textContent= "19 813 500"
+// })
+// btn8gb.addEventListener('click', ()=> {
+//     btn16gb.classList.remove('active')
+//     btn8gb.classList.add('active')
+//     btn1tb.classList.add('d-none')
+//     macPrice.textContent ="12 497 000"
+//     macPrice2.textContent= "14 621 000"
+// })
+// btn512gb.addEventListener('click', ()=> {
+//     btn256gb.classList.remove('active')
+//     btn1tb.classList.remove('active')
+//     btn512gb.classList.add('active')
+//     btn1tb.classList.remove('d-none')
+//     macPrice.textContent ="19 270 500"
+//     macPrice2.textContent= "22 546 500"
+//     count2 =1
+// })
+// btn1tb.addEventListener('click', ()=> {
+//     count2 =0
+//     btn512gb.classList.remove('active')
+//     btn1tb.classList.add('active')
+//     btn8gb.classList.add('d-none')
+//     btn16gb.classList.add('active')
+//     macPrice.textContent ="20 438 500"
+//     macPrice2.textContent= "23 913 000"
+//     // count2 =1
+//     if(count2==1) {
+//         btn512gb.classList.add('active')
+//         btn1tb.classList.remove('active')
+//         btn8gb.classList.remove('d-none')
+//         btn16gb.classList.remove('active')
+//     }
+// })
+// btn256gb.addEventListener('click', ()=> {
+//     btn512gb.classList.remove('active')
+//     btn256gb.classList.add('active')
+//     btn1tb.classList.remove('active')
+//     btn8gb.classList.remove('d-none')
+//     btn16gb.classList.remove('active')
+//     macPrice.textContent ="16 935 000"
+//     macPrice2.textContent= "19 813 500"
+// })
+
+// let plusBtn = document.querySelector('.plusBtn')
+// let minusBtn = document.querySelector('.minusBtn')
+// let macFullBtn =document.querySelector('.mac__full-btn')
+
+// plusBtn.addEventListener('click', ()=> {
+//     let a = macPrice.textContent*2
+//     console.log(a);
+//     macPrice.textContent = a
+//     macPrice2.textContent= `${macPrice/2}`
+// })
+// minusBtn.addEventListener('click', ()=> {
+//     let a = macPrice.textContent*2
+//     console.log(a);
+//     macPrice.textContent = a
+//     macPrice2.textContent= `${macPrice/2}`
+// })
--- js/test.js
+++ js/test.js
@@ -1,146 +1,195 @@
-const macBook = [
-    {
-        id: 1,
-        nomi: `Gold`,
-        imgUrl: "../images/macbookAir.webp",
-        imgSlider: ["../images/macbookAir.webp", "../images/macPink2.jpg", "../images/macPink3.jpg", "../images/macPink4.jpg", "../images/macPink5.jpg"],
-        ram: [
-            {
-                GB: 8,
-                xotira: [
-                    {
-                        name: `M1/8/256 Gold`,
-                        gbXotira: 256,
-                        price: 12497000
-                    },
-                    {
-                        name: `M1/8/512 Gold`,
-                        gbXotira: 512,
-                        price: 15066000
-                    }
-                ]
-            }, 
-            {
-                GB: 16,
-                xotira: [
-                    {
-                        name: `M1/16/256 Gold`,
-                        gbXotira: 256,
-                        price: `16 935 000`
-                    },
-                    {
-                        name: `M1/16/512 Gold`,
-                        gbXotira: 512,
-                        price: `19 270 500`
-                    },
-                    {
-                        name: `M1/16/1tb Gold`,
-                        gbXotira: 1,
-                        price: `22 497 000`
-                    }
-                ]
-            }
+const macObj = {
+    name: "Mac Book Air 13-inch",
+    ram: [
+      {
+        ramSize: 8,
+        active: false,
+        memory: [
+          {
+            active: true,
+            size: 256,
+            price: 12000000,
+          },
+          {
+            active: false,
+            size: 512,
+            price: 15000000,
+          },
         ],
-
-    },
-    {
-        id: 2,
-        nomi: `Silver`,
-        imgUrl: "../images/macSilver1.jpg",
-        imgSlider: ["../images/macSilver1.jpg", "../images/macSilver2.jpg", "../images/macSilver3.jpg", "../images/macSilver4.jpg", "../images/macSilver5.jpg"],
-        ram: [
-            {
-                GB: 8,
-                xotira: [
-                    {
-                        name: `M1/8/256 Silver`,
-                        gbXotira: 256,
-                        price: 12497000
-                    },
-                    {
-                        name: `M1/8/512 Silver`,
-                        gbXotira: 512,
-                        price: 15066000
-                    },
-                    {
-                        name: `M1/16/1tb Silver`,
-                        gbXotira: 1,
-                        price: `22 497 000`
-                    }
-                ]
-            }, 
-            {
-                GB: 16,
-                xotira: [
-                    {
-                        name: `M1/16/256 Silver`,
-                        gbXotira: 256,
-                        price: `16 935 000`
-                    },
-                    {
-                        name: `M1/16/512 Silver`,
-                        gbXotira: 512,
-                        price: `19 270 500`
-                    },
-                    {
-                        name: `M1/16/1tb Silver`,
-                        gbXotira: 1,
-                        price: `22 497 000`
-                    }
-                ]
-            }
+      },
+      {
+        ramSize: 16,
+        active: false,
+        memory: [
+          {
+            active: true,
+            size: 256 +"GB",
+            price: 17000000,
+          },
+          {
+            active: false,
+            size: 512 +"GB",
+            price: 20000000,
+          },
+          {
+            active: false,
+            size: 1 +"TB",
+            price: 21000000,
+          },
         ],
-
-    },
-    {
-        id: 3,
-        nomi: `Grey`,
-        imgUrl: "../images/macGrey1.jpg",
-        imgSlider: ["../images/macGrey1.jpg", "../images/macGrey2.jpg", "../images/macGrey3.jpg", "../images/macGrey4.jpg", "../images/macGrey5.jpg"],
-        ram: [
-            {
-                GB: 8,
-                xotira: [
-                    {
-                        name: `M1/8/256 Grey`,
-                        gbXotira: 256,
-                        price: 12497000
-                    },
-                    {
-                        name: `M1/8/512 Grey`,
-                        gbXotira: 512,
-                        price: 15066000
-                    }
-                ]
-            }, 
-            {
-                GB: 16,
-                xotira: [
-                    {
-                        name: `M1/16/256 Grey`,
-                        gbXotira: 256,
-                        price: `16 935 000`
-                    },
-                    {
-                        name: `M1/16/512 Grey`,
-                        gbXotira: 512,
-                        price: `19 270 500`
-                    },
-                    {
-                        name: `M1/16/1tb Grey`,
-                        gbXotira: 1,
-                        price: `22 497 000`
-                    }
-                ]
-            }
+      },
+    ],
+    colors: [
+      {
+        name: "Gold",
+        img: ['../images/macbookAir.webp',
+      '../images/macPink2.jpg', '../images/macPink3.jpg','../images/macPink4.jpg', '../images/macPink5.jpg' ],
+      },
+      {
+        name: "Silver",
+        img: [
+          '../images/macSilver1.jpg', '../images/macSilver2.jpg', '../images/macSilver3.jpg', '../images/macSilver4.jpg', '../images/macSilver5.jpg'
         ],
+      },
+      {
+        name: "Gray color",
+        img: [
+          '../images/macGrey1.jpg', '../images/macGrey2.jpg', '../images/macGrey3.jpg', '../images/macGrey4.jpg', '../images/macGrey5.jpg'
+        ],
+      },
+    ],
+  };
 
+let sanoq =0
+let macRamHotira = document.querySelector('.mac__ram-hotira')
+let macRamBox = document.querySelector('.mac__ram-box')
+let macRamColor = document.querySelector('.mac__ram-color')
+let macTitle = document.querySelector('.mac__title')
+let macPrice = document.querySelector('.mac__price')
+let macPrice2 = document.querySelector('.mac__price2')
+
+macObj.ram.forEach((item)=> {
+
+    let button = document.createElement('button');
+    button.className = "mac__ram-btn ram";
+    button. innerHTML = `
+    ${item.ramSize}GB
+  `
+  macRamBox.appendChild(button)
+})
+
+
+macObj.colors.forEach((item)=> {
+
+        let button = document.createElement('button');
+        button.className = "mac__ram-btn color";
+        button.innerHTML = 
+        `
+        <span class="color1"></span> ${item.name}`
+
+  macRamColor.appendChild(button)
+})
+macObj.ram[1].memory.forEach((item)=> {
+
+  let button = document.createElement('button');
+  button.className = "mac__ram-btn ramSize";
+  button.innerHTML = `
+  ${item.size} `
+
+  macRamHotira.appendChild(button)
+})
+
+let rams = document.querySelectorAll('.ram')
+rams[0].classList.add('active')
+
+rams[0].addEventListener(('click'), () => {
+
+  rams[0].classList.add('active')
+  rams[1].classList.remove('active')
+  macTitle.innerHTML = `MacBook Air 13-inch M1/8/256 Gold`
+  ramSize[2].style.display= "none"
+  macPrice.innerHTML =`12 497 000`;
+  macPrice2.innerHTML =`14 621 000`
+})
+rams[1].addEventListener(('click'), () => {
+  rams[1].classList.add('active')
+  rams[0].classList.remove('active')
+  macTitle.innerHTML =`MacBook Air 13-inch M1/16/256 Gold`
+  ramSize[2].style.display= "block"
+  macPrice.innerHTML =`16 935 000`;
+  macPrice2.innerHTML =` 19 813 500`
+})
+
+let ramSize = document.querySelectorAll('.ramSize')
+
+ramSize[2].style.display= "none"
+ramSize[0].classList.add('active');
+
+ramSize[0].addEventListener(('click'), () => {
+  ramSize[0].classList.add('active')
+  ramSize[1].classList.remove('active')
+  ramSize[2].classList.remove('active')
+  rams[0].classList.remove('d-none')
+  macTitle.innerHTML =`MacBook Air 13-inch M1/16/256 Gold`
+})
+ramSize[1].addEventListener(('click'), () => {
+  ramSize[1].classList.add('active')
+  ramSize[0].classList.remove('active')
+  ramSize[2].classList.remove('active')
+  ramSize[2].style.display= "block"
+  macTitle.innerHTML =`MacBook Air 13-inch M1/16/512 Gold`
+  macPrice.innerHTML = `19 270 500`
+  macPrice2.innerHTML = `22 546 500`
+})
+ramSize[2].addEventListener(('click'), () => {
+  ramSize[2].classList.add('active')
+  ramSize[0].classList.remove('active')
+  ramSize[1].classList.remove('active')
+  rams[0].classList.add('d-none')
+  rams[1].classList.add('active')
+  macTitle.innerHTML =`MacBook Air 13-inch
+  M1/16/1tb Gold`
+  macPrice.innerHTML = `20 438 500`
+  macPrice2.innerHTML = ` 23 913 000`
+})
+
+
+let colors = document.querySelectorAll('.color');
+colors[0].classList.add('active')
+
+  colors[0].addEventListener('click', ()=> {
+    colors[0].classList.add('active')
+    colors[1].classList.remove('active')
+    colors[2].classList.remove('active')
+  })
+  colors[1].addEventListener('click', ()=> {
+    colors[1].classList.add('active')
+    colors[0].classList.remove('active')
+    colors[2].classList.remove('active')
+    if(ramSize[2].active == true) {
+      console.log(ok);
     }
-];
+  })
+  colors[2].addEventListener('click', ()=> {
+    colors[2].classList.add('active')
+    colors[0].classList.remove('active')
+    colors[1].classList.remove('active')
+  })
 
 
-// for (let i=0; i<macBook.length; i++) {
-//     for(let j =i; macBook[0]; j++) {
-//         console.log(macBook[j]);
-//     }
-// }
+let plusBtn = document.querySelector('.plusBtn')
+let numberInput = document.querySelector('.numberInput')
+let minusBtn = document.querySelector('.minusBtn')
+
+
+let macSliderBox= document.querySelector('.mac__slider-box')
+
+macObj.colors.forEach((item) => {
+  console.log(item.img);
+  let img = document.createElement('img');
+  // img.className = "mac__img";
+  img.innerHTML = `
+  ${item.img}`
+  macSliderBox.appendChild(img)
+
+})
